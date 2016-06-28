752ab0ce5f93f07e9cd752bc9a55e08e11c6e7f3
Olivia Hinton and Stephanie Gurung <olivia@>
2016-06-28 09:29:00 -0700

    spec manages all uppercase entries and mixed case entries

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -8,4 +8,10 @@ describe('scrabble') do
   it("returns a scrabble score for a word") do
     expect("cat".scrabble()).to(eq(5))
   end
+  it("manages all uppercase entries") do
+    expect("CAT".scrabble()).to(eq(5))
+  end
+  it("manages mixed case entries") do
+    expect("CaT".scrabble()).to(eq(5))
+  end
 end
