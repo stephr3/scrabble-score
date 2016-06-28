521cd76b5a704cc0f97859611c8523dedba9a3f9
Olivia Hinton and Stephanie Gurung <stephanie@>
2016-06-28 09:22:49 -0700

    returns a scrabble score for a word

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,7 @@ describe('scrabble') do
   it("returns a scrabble score for a letter") do
     expect("a".scrabble()).to(eq(1))
   end
+  it("returns a scrabble score for a word") do
+    expect("cat".scrabble()).to(eq(5))
+  end
 end
